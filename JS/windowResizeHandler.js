e02e408960c64f9cce49503ffa235440c6149e08
Circuit-Overtime <ayushbhatt633@gmail.com>
2024-02-11 18:09:15 +0530

    added mobile responsive view

--- JS/windowResizeHandler.js
+++ JS/windowResizeHandler.js
@@ -9,12 +9,16 @@ function windowResizeHandler()
         document.querySelector(".bodyContainer").style.transform = "translateX(-50%) translateY(-50%) scaleX("+1+") scaleY("+1+")";
         // console.log(scaleXValue, scaleYValue)
     }
-    else if((windowHeight != 768) && (windowWidth != 1366))
+    else if((windowHeight > 768) && (windowWidth > 1366))
     {
         scaleXValue = (windowWidth/1349).toFixed(8)
         scaleYValue = (windowHeight/641).toFixed(8)
         document.querySelector(".bodyContainer").style.transform = "translateX(-50%) translateY(-50%) scaleX("+scaleXValue+") scaleY("+scaleYValue+")";
         // console.log(scaleXValue, scaleYValue)
+    }
+    else 
+    {
+        document.querySelector(".bodyContainer").style.transform = "translateX(-50%) translateY(-50%) scaleX("+1+") scaleY("+1+")";
     }
 
         
